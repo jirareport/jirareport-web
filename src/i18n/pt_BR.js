b7feb949e117ba67773eab109748efaad12ce4f8
Leonardo Ferreira <mail@leonardoferreira.com.br>
2023-09-25 23:59:42 -0300

    boards

--- src/i18n/pt_BR.js
+++ src/i18n/pt_BR.js
@@ -16,6 +16,30 @@ export default {
             button: 'Filtrar'
         },
         nameLabel: "Nome",
-        ownerLabel: "Dono"
+        ownerLabel: "Dono",
+        empytBoardList: 'Nenhum board encontrado',
+        remove: {
+            success: "Board removido com sucesso",
+            failure: "Falha ao remover o board"
+        },
+        clone: {
+            success: 'Board duplicado com sucesso',
+            failure: 'Falha ao duplicar o board'
+        },
+        dropdown: {
+            actions: {
+                edit: "Configurações",
+                dynamicFieldConfigs: "Campos Dinâmicos",
+                leadTimeConfigs: "Configurações de Lead Times",
+                holidays: "Feriados",
+                clone: "Duplicar",
+                remove: "Remover"
+            }
+        },
+        buttons: {
+            estimates: "Previsibilidade",
+            issues: "SandBox",
+            issuePeriods: "Períodos"
+        }
     }
 }
--- src/pages/Board/ListBoard/CardBoard/CardBoard.js
+++ src/pages/Board/ListBoard/CardBoard/CardBoard.js
@@ -8,6 +8,8 @@ import { AlertService, HttpService, NotificationService } from "services";
 import { Button, Col } from "components/ui";
 import { Link } from "react-router-dom";
 
+import t from 'i18n/i18n'
+
 import M from "materialize-css";
 
 import "./CardBoard.scss";
@@ -36,15 +38,15 @@ class CardBoard extends Component {
             const willDelete = await AlertService.confirmRemove();
             if (willDelete) {
                 await HttpService.delete(`/boards/${board.id}`);
-                NotificationService.notifySuccess("Board removido com sucesso");
+                NotificationService.notifySuccess(t('boards.remove.success'));
                 this.props.refreshBoards();
             }
         } catch (e) {
-            NotificationService.notifyError("Falha ao remover o board");
+            NotificationService.notifyError(t('boards.remove.failure'));
         }
     };
 
-    copyBoard = async () => {
+    cloneBoard = async () => {
         const { board } = this.props;
 
         try {
@@ -54,10 +56,10 @@ class CardBoard extends Component {
                 }
             });
 
-            NotificationService.notifySuccess("Board duplicado com sucesso");
+            NotificationService.notifySuccess(t('boards.clone.success'));
             this.props.refreshBoards();
         } catch (e) {
-            NotificationService.notifyError("Falha ao duplicar o board");
+            NotificationService.notifyError(t('boards.clone.failure'));
         }
     };
 
@@ -73,33 +75,47 @@ class CardBoard extends Component {
                     </Link>
                     <ul id={"board-dropdown-" + board.id} className="dropdown-content">
                         <li>
-                            <Link to={`/boards/${board.id}/edit`}>Configurações</Link>
+                            <Link to={`/boards/${board.id}/edit`}>
+                                {t('boards.dropdown.actions.edit')}
+                            </Link>
                         </li>
                         <li>
-                            <Link to={`/boards/${board.id}/dynamic-field-configs`}>Campos Dinâmicos</Link>
+                            <Link to={`/boards/${board.id}/dynamic-field-configs`}>
+                                {t('boards.dropdown.actions.dynamicFieldConfigs')}
+                            </Link>
                         </li>
                         <li>
-                            <Link to={`/boards/${board.id}/lead-time-configs`}>Configurações de Lead Times</Link>
+                            <Link to={`/boards/${board.id}/lead-time-configs`}>
+                                {t('boards.dropdown.actions.leadTimeConfigs')}
+                            </Link>
                         </li>
                         <li>
-                            <Link to={`/boards/${board.id}/holidays`}>Feriados</Link>
+                            <Link to={`/boards/${board.id}/holidays`}>
+                                {t('boards.dropdown.actions.holidays')}
+                            </Link>
                         </li>
                         <li>
-                            <Button link onClick={this.copyBoard}>
-                                Duplicar
+                            <Button link onClick={this.cloneBoard}>
+                                {t('boards.dropdown.actions.clone')}
                             </Button>
                         </li>
                         {userConfig.username === board.owner && <li>
                             <Button link onClick={this.deleteBoard}>
-                                Remover
+                                {t('boards.dropdown.actions.remove')}
                             </Button>
                         </li>}
                     </ul>
                 </div>
                 <div className="card-action right-align">
-                    <Link to={`/boards/${board.id}/estimates`}>Previsibilidade</Link>
-                    <Link to={`/boards/${board.id}/issues`}>SandBox</Link>
-                    <Link to={`/boards/${board.id}/issue-periods`}>Períodos</Link>
+                    <Link to={`/boards/${board.id}/estimates`}>
+                        {t('boards.buttons.estimates')}
+                    </Link>
+                    <Link to={`/boards/${board.id}/issues`}>
+                        {t('boards.buttons.issues')}
+                    </Link>
+                    <Link to={`/boards/${board.id}/issue-periods`}>
+                        {t('boards.buttons.issuePeriods')}
+                    </Link>
                 </div>
             </div>
         </Col>;
